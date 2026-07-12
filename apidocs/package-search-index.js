d7211dd26277cc02a4e2ffe755f78dedfa1b9b3a
CI Server <ci@example.com>
2026-07-12 08:46:52 +0000

    Site checkin for project OpenWMS.org CORE: Administration UI

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.openwms.core.admin"},{"l":"org.openwms.core.admin.app"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.openwms.core.admin"},{"l":"org.openwms.core.admin.app"}];updateSearchResults();
